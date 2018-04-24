3500d6e461eb1a151acc183ea0cfff3cf08e4023
lndr27 <lndr27@hotmail.com>
2018-04-23 22:31:14 -0300

    updating dependencies

--- Lndr.Simple.GUI/renderer.js
+++ Lndr.Simple.GUI/renderer.js
@@ -1,7 +1,7 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-var edge = require('electron-edge');
+var edge = require('electron-edge-js');
 var dialog = require('electron').remote.dialog;
 
 var assemblyFile = 'Lndr.Simple.CLR.dll';
