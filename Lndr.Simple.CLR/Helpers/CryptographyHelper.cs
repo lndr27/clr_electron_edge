8a04c10990008d3550520e14a000ff4f09a6c8e8
lndr27 <lndr27@hotmail.com>
2018-07-06 21:41:35 -0300

    CryptographyHelper decryptor

--- Lndr.Simple.CLR/Helpers/CryptographyHelper.cs
+++ Lndr.Simple.CLR/Helpers/CryptographyHelper.cs
@@ -8,29 +8,47 @@ namespace Lndr.Simple.CLR.Helpers
 {
     public static class CryptographyHelper
     {
-        public static byte[] AES_Encrypt(byte[] bytesToBeEncrypted, string publicKey, byte[] encryptedBytesHeader = null)
+        #region Constants +
+        const int HEADER_LENGTH              = 50;
+        const int RSA_RESULT_LENGTH          = 128;
+        const int RSA_KEY_LENGTH             = 2048;
+        const int PASSWORD_RANDOM_ITERATIONS = 1000;
+        const int PASSWORD_LENGTH            = 64;
+        const int SALT_LENGTH                = 32;
+        #endregion
+
+        #region Encryption +
+        public static byte[] Encrypt(byte[] bytesToBeEncrypted, string publicKey)
+        {
+            return Encrypt(bytesToBeEncrypted, publicKey, string.Empty);
+        }
+
+        public static byte[] Encrypt(byte[] bytesToBeEncrypted, string publicKey, string encryptedBytesHeader)
         {
             Guard.ForArgumentNull(bytesToBeEncrypted, "bytesToBeEncrypted");
             Guard.ForArgumentNullOrEmpty(publicKey, "publicKey");
+            Guard.ForArgumentNullOrEmpty(encryptedBytesHeader, "encryptedBytesHeader");
 
-            var header = new byte[50];
-            if (encryptedBytesHeader != null)
-            {
-                System.Buffer.BlockCopy(encryptedBytesHeader, 0, header, 0, encryptedBytesHeader.Length);
-            }
-            else
-            {
-                Array.Clear(header, 0, header.Length);
-            }
+            var header            = encryptedBytesHeader.PadRight(HEADER_LENGTH, ' ').GetBytes();
+            var password          = Membership.GeneratePassword(PASSWORD_LENGTH, PASSWORD_LENGTH / 2).GetBytes();
+            var encryptedPassword = RSA_Encryption(password, publicKey);
+            var salt              = GenerateRandomSalt();
+            var encryptedSalt     = RSA_Encryption(salt, publicKey);
+            var encryptedData     = AES_Encrypt(bytesToBeEncrypted, publicKey, header, password, salt);
+            return header.Combine(encryptedPassword, encryptedSalt, encryptedData);
+        }
+
+        private static byte[] AES_Encrypt(byte[] bytesToBeEncrypted, string publicKey, byte[] header, byte[] passwordBytes, byte[] saltBytes)
+        {
+            Guard.ForArgumentNull(bytesToBeEncrypted, "bytesToBeEncrypted");
+            Guard.ForArgumentNullOrEmpty(publicKey, "publicKey");
+            Guard.ForArgumentNull(header, "header");
 
             using (var ms = new MemoryStream())
             {
                 using (var AES = new RijndaelManaged())
                 {
-                    var randomPasswordBytes = Membership.GeneratePassword(64, 32).GetBytes();
-                    var saltBytes = GenerateRandomSalt();
-                    var key = new Rfc2898DeriveBytes(randomPasswordBytes, saltBytes, 1000);
-
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, PASSWORD_RANDOM_ITERATIONS);
                     AES.KeySize = 256;
                     AES.BlockSize = 128;
                     AES.Key = key.GetBytes(AES.KeySize / 8);
@@ -42,18 +60,14 @@ namespace Lndr.Simple.CLR.Helpers
                         cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
                         cs.Close();
                     }
-
-                    var encryptedPassword = RSA_Encryption(randomPasswordBytes, publicKey);
-                    var encryptedSalt = RSA_Encryption(saltBytes, publicKey);
-                    
-                    return encryptedBytesHeader.Combine(encryptedPassword, encryptedSalt, ms.ToArray());
+                    return ms.ToArray();
                 }
             }
         }
 
         private static byte[] GenerateRandomSalt()
         {
-            var data = new byte[32];
+            var data = new byte[SALT_LENGTH];
 
             using (var rng = new RNGCryptoServiceProvider())
             {
@@ -67,7 +81,7 @@ namespace Lndr.Simple.CLR.Helpers
 
         private static byte[] RSA_Encryption(byte[] input, string publicKey)
         {
-            using (var rsa = new RSACryptoServiceProvider(2048))
+            using (var rsa = new RSACryptoServiceProvider(RSA_KEY_LENGTH))
             {
                 try
                 {
@@ -80,5 +94,65 @@ namespace Lndr.Simple.CLR.Helpers
                 }
             }
         }
+        #endregion
+
+        #region Decryption +
+        public static byte[] Decrypt(byte[] bytesToBeDecrypted, string privateKey)
+        {
+            var header = new byte[HEADER_LENGTH];
+            var passwordBytesEncrypted = new byte[RSA_RESULT_LENGTH];
+            var saltBytesEncrypted = new byte[RSA_RESULT_LENGTH];
+            var encryptedData = new byte[bytesToBeDecrypted.Length - (RSA_RESULT_LENGTH * 2) - HEADER_LENGTH]; 
+
+            Buffer.BlockCopy(bytesToBeDecrypted, 0, header, 0, header.Length);
+            Buffer.BlockCopy(bytesToBeDecrypted, HEADER_LENGTH, passwordBytesEncrypted, 0, passwordBytesEncrypted.Length);
+            Buffer.BlockCopy(bytesToBeDecrypted, HEADER_LENGTH + RSA_RESULT_LENGTH, saltBytesEncrypted, 0, saltBytesEncrypted.Length);
+            Buffer.BlockCopy(bytesToBeDecrypted, HEADER_LENGTH + (RSA_RESULT_LENGTH * 2), encryptedData, 0, encryptedData.Length);
+
+            var passwordBytes = RSA_Decryption(passwordBytesEncrypted, privateKey);
+            var saltBytes = RSA_Decryption(saltBytesEncrypted, privateKey);
+
+            return AES_Decrypt(encryptedData, passwordBytes, saltBytes, privateKey);
+        }
+
+        private static byte[] AES_Decrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes, byte[] saltBytes, string privateKey)
+        {
+            using (var ms = new MemoryStream())
+            {
+                using (var AES = new RijndaelManaged())
+                {
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, PASSWORD_RANDOM_ITERATIONS);
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cs.Close();
+                    }
+                    return ms.ToArray();
+                }
+            }
+        }
+
+        private static byte[] RSA_Decryption(byte[] bytesToDecrypt, string privateKey)
+        {
+            using (var rsa = new RSACryptoServiceProvider(RSA_KEY_LENGTH))
+            {
+                try
+                {
+                    rsa.FromXmlString(privateKey);
+                    return rsa.Decrypt(bytesToDecrypt, true);
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
+        }
+        #endregion
     }
 }
--- Lndr.Simple.Console/Program.cs
+++ Lndr.Simple.Console/Program.cs
@@ -14,12 +14,11 @@ namespace Lndr.Simple.Console
         static void Main(string[] args)
         {
             var pubKey = @"<RSAKeyValue><Modulus>3tR+wUpHkv/dsrg2MHCVo3xlAJlF/K12VoWD3iSEyjWNwvu+tYnkG6gKoVVssVv+oLlPpdDqtTE9B36eXgf9X+INUaXW5IR9hJL9s/vjbOj5KZlIOHFrQDubIjo5djj1Z3nmRdQIkGwuyF6C7Sz+Svipu4Ermc5Rdfj02YEOcPk=</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
+            var privateKey = @"<RSAKeyValue><Modulus>3tR+wUpHkv/dsrg2MHCVo3xlAJlF/K12VoWD3iSEyjWNwvu+tYnkG6gKoVVssVv+oLlPpdDqtTE9B36eXgf9X+INUaXW5IR9hJL9s/vjbOj5KZlIOHFrQDubIjo5djj1Z3nmRdQIkGwuyF6C7Sz+Svipu4Ermc5Rdfj02YEOcPk=</Modulus><Exponent>AQAB</Exponent><P>/0jLDdULItLk3eYaO3fEia00riKNynJuvPxbcgeZahvHR102nbR6lveLVhKYmtKkm0W2AAf7Bn7qGd4Uw4/6Rw==</P><Q>33RpL4IlhnCJgranFAHLnwKApRSiLqOOVqbxT9o0AnI8ZWhBtF1S3W9aNbSaXbhh1VrvYeaXMA/UpfRaWUyavw==</Q><DP>n5rVSOqfscAE0y8fy3pP6Hvf6dc0qxPsZD+qtJpHsm13pAiFMwg5dNWDyeaKfpGf1Gw7I73ZlfAXUODmPnZv1Q==</DP><DQ>II/IHCIs8bBIeYHASwwP7fXhZSzijUVMIphlJau4VHrtDiHpIS1QO/BUButwayPJLdv4ch21/kzwQdafq8+b5Q==</DQ><InverseQ>XQaUBilkhuMaRaoZifJTjvVBn8ajQBs2GrZoXShlGe/QW3hwRVz5Ej8wiwjfL2ZbEEheYWGz7D6ayEZm7SepFw==</InverseQ><D>QxK1gNsqBj6MbjJy5UvOTyKviGmann9cQpj345McgX1zSYDbm+uAxS5k3MFI8W4ejCSbq+7h516F306mvukqw/S9VczRu8K+jlXlxsZNK0yaD6zTjbgi+g7607/gqwe0C1+AgzuemntZKJaEOX12QOetzIHkrfdpfhttl5bHR00=</D></RSAKeyValue>";
             var header = @"LNDR.TESTE";
-            var encryptedData = CryptographyHelper.AES_Encrypt("NAO NOA NONFSKDFJ SDFKJ:SDKJF SD:LKFJ ".GetBytes(), pubKey, header.GetBytes());
+            var encryptedData = CryptographyHelper.Encrypt("SOU LINDO".GetBytes(), pubKey, header);
 
-            File.WriteAllBytes(@"C:\\users\lndr2\Desktop\test.lndr", encryptedData);
-
-            //var classe = new EventosController();
+            var descryptedData = CryptographyHelper.Decrypt(encryptedData, privateKey);
 
             //classe.AdicionarPacoteEventosAsync(new string[] { @"C:\\users\lndr2\desktop\teste.reinf" }).Wait();
 
